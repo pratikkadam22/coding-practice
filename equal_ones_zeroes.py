1caef89450c869a2bda63bc972228a09af758bdf
pratikkadam22 <pratikkadam2212@gmail.com>
2020-09-23 14:01:38 -0700

    Equal 0s and 1s

---
+++ equal_ones_zeroes.py
@@ -0,0 +1,26 @@
+# Find the longest subarray where:
+# number of values greater than equal to 10 = number of values less than 10
+
+def equal_ones_zeroes(input, n):
+    curr_sum, maxlen, end_index = 0, 0, 0
+    hashmap = {}
+    for i in range(n):
+        if input[i] >= 10:
+            curr_sum += 1
+        else:
+            curr_sum -= 1
+        if curr_sum == 0:
+            maxlen = i - 1
+            end_index = i
+        if curr_sum in hashmap:
+            if i - hashmap[curr_sum] > maxlen:
+                maxlen = i - hashmap[curr_sum]
+                end_index = i
+        else:
+            hashmap[curr_sum] = i
+    print(input[end_index: end_index - maxlen: -1])
+    return maxlen
+ 
+input = [1,5,6,10,23,14,2,5,29,2,1]
+n = len(input)     
+print(equal_ones_zeroes(input, n))
