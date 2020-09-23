161c21db10c9e57a2b40e3c488fad256197effc3
pratikkadam22 <pratikkadam2212@gmail.com>
2020-09-23 16:16:00 -0700

    equal ones and zeroes

--- equal_ones_zeroes.py
+++ equal_ones_zeroes.py
@@ -10,7 +10,7 @@ def equal_ones_zeroes(input, n):
         else:
             curr_sum -= 1
         if curr_sum == 0:
-            maxlen = i - 1
+            maxlen = i + 1
             end_index = i
         if curr_sum in hashmap:
             if i - hashmap[curr_sum] > maxlen:
@@ -18,9 +18,12 @@ def equal_ones_zeroes(input, n):
                 end_index = i
         else:
             hashmap[curr_sum] = i
+    if maxlen == len(input):
+        print(input)
+        return maxlen
     print(input[end_index: end_index - maxlen: -1])
     return maxlen
  
-input = [1,5,6,10,23,14,2,5,29,2,1]
+input = [1,5,6,10,23,14,2,5,29,2,1,12]
 n = len(input)     
 print(equal_ones_zeroes(input, n))
